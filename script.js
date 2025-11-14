591eeef4b4fba33d5ac73e6d326120145474d03c
Bianeta <bianetaugwu@gmail.com>
2025-11-13 20:29:10 -0500

    started js page

--- script.js
+++ script.js
@@ -7,27 +7,62 @@ var saturnScore=0;
 var uranusScore=0;
 var neptuneScore=0;
 var murcuryScore=0;
-var plutoScore=0;
-var ceresScore=0;
-var HaumeaScore=0;
-var makemakeScore=0;
-var erisScore=0;
+var dwarfScore=0;
 
 
 
 var button = document.getElementById("restart");
 var q1a1 = document.getElementById("q1a1");
 var q1a2 = document.getElementById("q1a2");
+var q1a3 = document.getElementById("q1a3");
+
 
 var q2a1 = document.getElementById("q2a1");
 var q2a2 = document.getElementById("q2a2");
+var q2a3 = document.getElementById("q2a3");
+var q2a4 = document.getElementById("q2a4");
 
 var q3a1 = document.getElementById("q3a1");
 var q3a2 = document.getElementById("q3a2");
+var q3a3 = document.getElementById("q3a3");
+
+var q4a1 = document.getElementById("q4a1");
+var q4a2 = document.getElementById("q4a2");
+var q4a3 = document.getElementById("q4a3");
+
+var q5a1 = document.getElementById("q5a1");
+var q5a2 = document.getElementById("q5a2");
+var q5a3 = document.getElementById("q5a3");
+
+var q6a1 = document.getElementById("q6a1");
+var q6a2 = document.getElementById("q6a2");
+var q6a3 = document.getElementById("q6a3");
+
+var q7a1 = document.getElementById("q7a1");
+var q7a2 = document.getElementById("q7a2");
+var q7a3 = document.getElementById("q7a3");
+
 var result = document.getElementById("result");
 
-q1a1.addEventListener("click", sword);
-q1a2.addEventListener("click", spell);
+q1a1.addEventListener("click", neptune);
+q1a1 .addEventListener("click", dwarf);
+q1a1.addEventListener("click", uranus);
+
+
+q1a2.addEventListener("click", jupiter);
+q1a2.addEventListener("click", saturn);
+q1a2.addEventListener("click", thesun);
+
+
+q1a3.addEventListener("click", earth);
+q1a3.addEventListener("click", mars);
+q1a3.addEventListener("click", venus);
+q1a3.addEventListener("click", murcury);
+
+
+
+
+
 
 q2a1.addEventListener("click", sword);
 q2a2.addEventListener("click", spell);
